018b002d00fa371fe3f78edd79d9225570ce1f83
adumbidiot <adumbidiot@users.noreply.github.com>
2024-03-27 05:08:48 +0000

    deploy: 049da36e6d2797970202084802257e9a7bfb560b

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["itch_io",["",[["types",[],["download_page.rs","game_page.rs","purchase_dialog.rs"]]],["client.rs","lib.rs","types.rs"]]]\
+]'));
+createSrcSidebar();
